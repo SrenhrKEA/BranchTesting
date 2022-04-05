297fa4fd8b2a5a6ce55705aa8f59f88668e9bc06
SrenhrKEA <soer53k0@stud.kea.dk>
2022-04-05 14:18:32 +0200

    Fourth Commit

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,12 @@
 public class Test {
+  public static void main(String[] args) {
+    new Test().runProgram();
+  }
+
+  public void runProgram() {
+    Cat cat = new Cat("Pussycat");
+    cat.makeSound();
+    Dog dog = new Dog("Rollo");
+    dog.makeSound();
+  }
 }
